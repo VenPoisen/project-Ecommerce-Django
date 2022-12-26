5e83f00960f9c3c266a00999d0c21815c1ba43a3
VenPoisen <alanmf.pro@gmail.com>
2022-12-26 20:50:09 +0100

    _nav.html update styles

---
+++ templates/static/vendor/js/index.js
@@ -0,0 +1,29 @@
+var toggleStatus = 1;
+var dropStatus = 1;
+
+function dropRight() {
+    if (dropStatus == 1 && toggleStatus == 0) {
+        document.getElementById("navbarDropleft").style.display = "block";
+        dropStatus = 0;
+    }
+    else if (dropStatus == 0 && toggleStatus == 0) {
+        document.getElementById("navbarDropleft").style.display = "none";
+        dropStatus = 1;
+    }
+};
+
+function toggleMenu() {
+    if (toggleStatus == 1) {
+        document.getElementById("menu").style.left = "0";
+        document.getElementById("block-outside").style.display = "block";
+        toggleStatus = 0;
+    }
+    else if (toggleStatus == 0) {
+        document.getElementById("menu").style.left = "-500px";
+        document.getElementById("block-outside").style.display = "none";
+        document.getElementById("navbarDropleft").style.display = "none";
+        dropStatus = 1;
+        toggleStatus = 1;
+    }
+};
+
