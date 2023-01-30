3877dfc1cc911d103243ab00dacfc364e71b9360
VenPoisen <alanmf.pro@gmail.com>
2023-01-30 21:36:05 +0100

    Creating views for multiple addresses
    Now a user can have multiple addresses linked to their profile.
    This way, when placing an order, he can choose the best address for delivery.

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,15 @@
+from crispy_forms.templatetags.crispy_forms_filters import as_crispy_field
+from django.http import JsonResponse
+from utils import addressgenerator
+from .forms import AddressForm
+from django.views.generic import ListView, UpdateView
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import ListView
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponseNotFound, HttpResponse
 from . import models, forms
 import copy
 
@@ -28,41 +33,40 @@ class BaseProfile(View):
             self.address = models.Address.objects.filter(
                 user=self.profile).all()
 
-            # For now using the first address to show up in the form.
-            instance_address = self.address.first()
-
-            # TODO: the idea is to show all the adresses using a for like this one:
-            # for i in range(0, self.address.count()):
-            #     print(self.address[i])
+            self.address_id = kwargs.get("pk", None)
+            self.address_filter = self.address.filter(
+                id=self.address_id).first()
 
             self.context = {
                 'userform': forms.UserForm(data=self.request.POST or None, user=self.request.user, instance=self.request.user,),
                 'profileform': forms.ProfileForm(data=self.request.POST or None, instance=self.profile),
-                # Must pass the 'instance=' in the POST addressform as well. Otherwise a new address will be created, rather than updating the old one. # TODO: observation for creating multiple addresses.
-                'addressform': forms.AddressForm(data=self.request.POST or None, instance=instance_address),
+                'addressform': forms.AddressForm(data=self.request.POST or None),
+                'addresses': self.address,
+                'addressdetails': forms.AddressForm(
+                    data=self.request.POST or None, instance=self.address_filter),
             }
+
+            self.addressdetails = self.context['addressdetails']
+
         else:
             self.context = {
                 'userform': forms.UserForm(data=self.request.POST or None),
                 'profileform': forms.ProfileForm(data=self.request.POST or None),
                 'addressform': forms.AddressForm(data=self.request.POST or None),
-
             }
 
         self.userform = self.context['userform']
         self.profileform = self.context['profileform']
         self.addressform = self.context['addressform']
 
-        self.render = render(self.request, self.template_name, self.context)
-
     def get(self, *args, **kwargs):
-        return self.render
+        return render(self.request, self.template_name, self.context)
 
 
 class Create(BaseProfile):
     def post(self, *args, **kwargs):
 
-        if not self.userform.is_valid() or not self.profileform.is_valid() or not self.addressform.is_valid():
+        if not self.userform.is_valid() or not self.profileform.is_valid():
             messages.error(
                 self.request,
                 'There are errors in the register form. Check if all fields have been filled correctly.'
@@ -74,7 +78,6 @@ class Create(BaseProfile):
         email = self.userform.cleaned_data.get('email')
         first_name = self.userform.cleaned_data.get('first_name')
         last_name = self.userform.cleaned_data.get('last_name')
-        address_data = self.addressform.cleaned_data
 
         cpf_data = self.profileform.cleaned_data.get('cpf')
         cpf_db = models.Profile.objects.filter(cpf=cpf_data).first()
@@ -106,25 +109,6 @@ class Create(BaseProfile):
                 profile.user = user
                 profile.save()
 
-            if not self.address:
-                self.addressform.cleaned_data['user'] = self.profile
-                address = models.Address(**self.addressform.cleaned_data)
-                address.save()
-            else:
-                address_exists = self.address.filter(
-                    address__iexact=address_data['address']).first()
-
-                if address_exists:
-                    if address_data['number'] != address_exists.number or address_data['neighborhood'] != address_exists.neighborhood or address_data['cep'] != address_exists.cep or address_data['city'] != address_exists.city or address_data['state'] != address_exists.state:
-                        address = self.addressform.save(commit=False)
-                        address.user = self.profile
-                        address.save()
-
-                else:
-                    address = self.addressform.save(commit=False)
-                    address.user = self.profile
-                    address.save()
-
             messages.info(
                 self.request,
                 'Your profile has been successfully updated'
@@ -132,6 +116,7 @@ class Create(BaseProfile):
 
             if self.request.session.get('cart'):
                 return redirect('products:checkout')
+            return self.render
 
         else:
             if cpf_db:
@@ -147,10 +132,6 @@ class Create(BaseProfile):
             profile.user = user
             profile.save()
 
-            address = self.addressform.save(commit=False)
-            address.user = profile
-            address.save()
-
             messages.info(
                 self.request,
                 'Your profile has been successfully created'
@@ -165,13 +146,174 @@ class Create(BaseProfile):
 
             if authentica:
                 login(self.request, user=user)
+                return redirect('profiles:address')
+        # TODO: em vez de redirecionar p um monte de lugar, criar um template do dashboard com os links?
+        self.request.session['cart'] = self.cart
+        self.request.session.save()
+
+        self.render = render(self.request, self.template_name, self.context)
+
+        if self.request.session['cart']:
+            return redirect('products:cart')
+        return self.render
+
+
+class DispatchLoginRequired(View):
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('profiles:create')
+
+        return super().dispatch(*args, **kwargs)
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        profile = models.Profile.objects.filter(
+            user=self.request.user).first()
+        qs = qs.filter(user=profile)
+        return qs
+
+
+class AddressUpdate(DispatchLoginRequired, BaseProfile):
+    address_template_name = 'profiles/updateaddress.html'
+
+    def get(self, *args, **kwargs):
+        if not self.address_filter:
+            return HttpResponseNotFound()
+
+        return render(self.request, self.address_template_name, self.context)
+
+    def post(self, request, *args, **kwargs):
+
+        if not self.addressdetails.is_valid():
+            messages.error(
+                self.request,
+                'There are errors in the register form. Check if all fields have been filled correctly.'
+            )
+            return render(self.request, self.address_template_name, self.context)
+
+        self.address_data = self.addressdetails.cleaned_data
+
+        if self.request.user.is_authenticated:
+
+            if not self.address:
+                self.addressdetails.cleaned_data['user'] = self.profile
+                address = models.Address(**self.addressdetails.cleaned_data)
+                address.save()
+            else:
+                address_exists = self.address.filter(
+                    cep__iexact=self.address_data['cep'], number__iexact=self.address_data['number']
+                ).first()
+
+                if address_exists:
+                    if self.address_data['complement'] != address_exists.complement:
+                        address = self.addressdetails.save(commit=False)
+                        address.user = self.profile
+                        address.save()
+
+                        messages.info(
+                            self.request,
+                            'Your address has been successfully updated'
+                        )
+                    else:
+                        messages.warning(
+                            self.request,
+                            'This address already exists in your profile.'
+                        )
+
+                        return redirect('profiles:addressdetails', self.address_id)
+                else:
+                    address = self.addressdetails.save(commit=False)
+                    address.user = self.profile
+                    address.save()
+
+                    messages.info(
+                        self.request,
+                        'Your address has been successfully updated'
+                    )
+
+            if self.request.session.get('cart'):
+                return redirect('products:checkout')
+
+            return redirect('profiles:address')
+
+        else:
+            return redirect('profiles:addresscreate')
+
+
+def cep_update(request, *args, **kwargs):
+    if request.method == 'GET':
+        form = forms.AddressForm(request.GET)
+
+        cep = request.GET['cep']
+
+        if cep == "":
+            return JsonResponse({"cep": "EMPTY CEP"})
+
+        cep_refresher = addressgenerator.address(cep)
+
+        if cep_refresher is None:
+            return HttpResponse(as_crispy_field(form['cep']))
+
+        changes = {
+            'address': cep_refresher['logradouro'],
+            'neighborhood': cep_refresher['bairro'],
+            'city': cep_refresher['cidade'],
+            'state': cep_refresher['uf'],
+        }
+
+        if cep_refresher['complemento'] != "":
+            if request.GET['complement'] == "":
+                changes.update({'complement': cep_refresher['complemento']})
+
+        return JsonResponse(changes)
+
+
+class AddressList(DispatchLoginRequired, ListView):
+    model = models.Address
+    context_object_name = 'addresses'
+    template_name = 'profiles/address.html'
+    paginate_by = 10
+    ordering = ['-id']
+
+
+class AddressCreate(BaseProfile):
+    address_template_name = 'profiles/newaddress.html'
+
+    def get(self, *args, **kwargs):
+        return render(self.request, self.address_template_name, self.context)
+
+    def post(self, *args, **kwargs):
+
+        if not self.addressform.is_valid():
+            messages.error(
+                self.request,
+                'There are errors in the register form. Check if all fields have been filled correctly.'
+            )
+            return render(self.request, self.address_template_name, self.context)
+
+        address = self.addressform.save(commit=False)
+        address.user = self.profile
+        address.save()
+
+        messages.info(
+            self.request,
+            'Your profile has been successfully created'
+        )
 
         self.request.session['cart'] = self.cart
         self.request.session.save()
 
         if self.request.session['cart']:
             return redirect('products:cart')
-        return self.render
+        return redirect('profiles:address')
+
+
+class AddressDelete(BaseProfile):
+
+    def get(self, *args, **kwargs):
+        self.address_filter.delete()
+
+        return redirect('profiles:address')
 
 
 class Login(View):
